367db023893b3e9384a9f72fd348ae02fddba7c8
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2022-12-12 18:12:56 -0300

    Delete back/src directory

--- back/src/config/index.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize'); 
-
-const database = new Sequelize('login','root','',{
-    dialect: 'mysql',
-    host:'localhost',
-    port: 3307,
-});
-
-module.exports = database;
